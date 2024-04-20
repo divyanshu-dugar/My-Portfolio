8171ebd346ae1c033f7356e71ce7dbc02e191bbb
Divyanshu Dugar <divyu2004@gmail.com>
2024-04-20 11:02:04 -0400

    added comments

--- JS/notes.js
+++ JS/notes.js
@@ -1,5 +1,6 @@
 let buttons = document.getElementsByClassName("download");
 
+// Adding event listener to all the buttons, to open the corresponding pdf in a new tab when clicked.
 for (const button of buttons) {
   button.addEventListener("click", function () {
     let pdfPath;
