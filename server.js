6ea97e7bdcc6806376616c9f62fd935ffe4f354d
Divyanshu Dugar <divyu2004@gmail.com>
2024-08-28 17:12:24 -0400

    added ejs and set the app to view views

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express();
 
-app.set('views', path)
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
