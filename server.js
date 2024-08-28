08dcbd8739b3012d1041ba77c414fe10a60c8a06
Divyanshu Dugar <divyu2004@gmail.com>
2024-08-28 17:02:33 -0400

    added all the routes and initial server setup

--- server.js
+++ server.js
@@ -8,3 +8,29 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
 const PORT = process.env.PORT || 8080;
+
+// Routes
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.get('/blogs',(req,res) => {
+    res.render('blogs');
+})
+
+app.get('/contact', (req, res) =>{
+    res.render('contact');
+})
+
+app.get('/notes', (req,res) => {
+    res.render('notes');
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+})
+
+app.listen(PORT,()=>{
+    console.log(`App Listening on port ${PORT}`)
+});
