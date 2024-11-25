bae41541b42a3a8630ce1e95206bd880cdb1d3d3
Divyanshu Dugar <divyu2004@gmail.com>
2024-11-25 14:27:17 -0500

    added routes for project2 UD operations

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 // const notesData = require('./data/notes.json');
+const methodOverride = require('method-override');
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({ extended: true })); // For parsing form data
+// override with POST having ?_method=DELETE / PATCH / PUT
+app.use(methodOverride('_method'))
 
 const PORT = process.env.PORT || 8080;
 
@@ -23,65 +26,160 @@ const blogsData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'blogs
 // projects.ejs page
 let projects = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'projects', 'projects-project-name.json')));
 let blogs = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'projects', 'projects-blogs-data.json'), 'utf-8'));
+let harryPotterData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'projects', 'projects-harry-potter.json'), 'utf-8'));
 
+//
+// --------------------------------------------------------------------
+//
 
 // Routes
 app.get('/',(req,res)=>{
     res.render('index');
 })
 
-app.get('/blogs',(req,res) => {
-    res.render('blogs', {blogs: blogCards})
-})
+//
+// --------------------------------------------------------------------
+//
 
 app.get('/contact', (req, res) =>{
     res.render('contact');
 })
 
+//
+// --------------------------------------------------------------------
+//
+
 app.get('/notes', (req, res) => {
   res.render('notes', { notes: notesData.notes });
 });
+
+//
+// --------------------------------------------------------------------
+//
+
+// --------------------
+// PROJECTS PAGE
+// --------------------
 
 app.get('/projects', (req, res) => {
     res.render('projects', {projects});
 })
 
-app.get('/projects/:id', (req, res) => {
-    let id = req.params.id;
-    let project = projects.find((project) => project.id === id)
-
-    if(id === "1"){
-        res.render('project1',{project});
-    }else if(id === "2"){
-        res.render('project2',{project,blogs});
-    }
+// PROJECT  1 - AJAX (JSON)
+app.get('/projects/1', (req, res) =>{
+    let project = projects[0];
+    res.render('project1', {project, harryPotterData});
 })
 
-// REST API - BLOGS
+// PROJECT 2 - (REST API - BLOGS)
+app.get('/projects/2', (req, res) =>{
+    let project = projects[1];
+    res.render('project2', {project, blogs});
+})
+
+// --------------------
+// C - ADDING NEW BLOGS
+// --------------------
+
 app.get('/project2/blogs/add', (req, res) => {
-    res.render('add');
+    res.render('./project2/add');
 })
 
 app.post('/project2/blogs/add', (req, res) => {
     let {name, content} = req.body;
-    let newBlog = {name, content};
-    console.log(newBlog);
+    // Generate a new unique ID based on existing blogs
+    let newId = (parseInt(blogs.length) + 1).toString();
 
-    
+    // Create a new blog object
+    let newBlog = { id: newId, name, content };
+
+    // Add the new blog to the blogs array
+    blogs.push(newBlog);
+
+    // Write the updated blogs array to the correct JSON file
+    const filePath = path.join(__dirname, 'data', 'projects', 'projects-blogs-data.json');
+    fs.writeFileSync(filePath, JSON.stringify(blogs, null, 2), 'utf-8');
+
+    res.redirect('/projects/2')
 })
 
+// --------------------------
+// R - FETCHING A REQUESTED BLOG
+// --------------------------
 app.get('/project2/blogs/:id', (req, res) => {
     let id = req.params.id;
-
-    // Ensure type compatibility between id and blog.id
     let blog = blogs.find((blog) => blog.id === id);
 
     if (!blog) {
         return res.status(404).send("Blog not found");
     }
-
-    res.render("show", { blog });
+    res.render("./project2/show", { blog });
 });
+
+// --------------------------
+// U - UPDATING A REQUESTED BLOG
+// --------------------------
+
+app.get('/project2/blogs/:id/update',(req, res)=>{
+    let id = req.params.id;
+    let blog = blogs.find((blog) => blog.id === id);
+
+    res.render('./project2/update',{blog});
+});
+
+app.patch('/project2/:id', (req, res) => {
+    let id = req.params.id;
+    let {name, content} = req.body;
+
+    let blog = blogs.find((blog) => blog.id === id);
+    blog.name = name;
+    blog.content = content;
+
+    blogs[id - 1] = blog;
+
+    const filePath = path.join(__dirname, 'data', 'projects', 'projects-blogs-data.json');
+    fs.writeFileSync(filePath, JSON.stringify(blogs, null, 2), 'utf-8');
+
+    res.redirect('/projects/2');
+})
+
+// --------------------------
+// D - DELETING THE BLOG
+// --------------------------
+app.delete('/project2/blogs/:id', (req, res) => {
+    let id = req.params.id;
+
+    blogs = blogs.filter((blog) => blog.id !== id);
+
+    const filePath = path.join(__dirname, 'data', 'projects', 'projects-blogs-data.json');
+    fs.writeFileSync(filePath, JSON.stringify(blogs, null, 2), 'utf-8');
+
+    res.redirect('/projects/2');
+})
+
+// Calling the requested project
+
+app.get('/projects/:id', (req, res) => {
+    let id = req.params.id;
+    
+    if(id === "1"){
+        res.redirect(`/project/${id}`)
+    }else if(id === "2"){
+        res.redirect(`/project/${id}`);
+    }
+})
+
+//
+// --------------------------------------------------------------------
+//
+
+// --------------------
+// BLOGS PAGE
+// --------------------
+
+app.get('/blogs',(req,res) => {
+    res.render('blogs', {blogs: blogCards})
+})
 
 app.get('/web-dev', (req, res)=>{
     res.render("web-dev", {webdevelopment});
